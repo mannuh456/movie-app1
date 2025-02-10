453c07a4c23b6108832de567e81ca17bb26a984d
mannuh456 <emmanuelmusyox@gmail.com>
2025-02-10 13:26:06 +0300

    add about page

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -6,7 +6,7 @@ export default function Providers({children}) {
   return (
     <div>
         <ThemeProvider defaultTheme='system'attribute='class'>
-            <div className='text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300'>
+            <div className='text-white-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300'>
                 {children}
             </div>
         </ThemeProvider>
