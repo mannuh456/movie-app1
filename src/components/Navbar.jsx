e527cb392b2edccd3895134c1c1293e9524ba2b1
mannuh456 <emmanuelmusyox@gmail.com>
2025-02-11 12:30:43 +0300

    create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import NavbarItem from "./NavbarItem";
+
+export default function Navbar() {
+  return (
+    <div className='flex bg-gray-600 p-4 lg:text-lg justify-center gap-4'>
+        <NavbarItem title='Trending' 
+        param='fetchTrending'/>
+        <NavbarItem title='Top Rated'
+        param='fetchTopRated'/>
+
+      
+    </div>
+  )
+}
